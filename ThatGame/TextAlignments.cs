f5b3c2cf70cb64ae6deacfbfa37e6a62276891d1
Xavier <xavier.richardson@onshoreoutsourcing.com>
2019-12-11 10:59:34 -0600

    Created content boxes and am working on the character creation page

--- ThatGame/TextAlignments.cs
+++ ThatGame/TextAlignments.cs
@@ -58,8 +58,99 @@ namespace ThatGame
             newLine();
         }
 
-        public void boxContent_SM(int margin = 0, int padding = 0) {
+        //Used to make content boxes on the screen
+        public void boxContent(List<string>content, int posX, int posY)
+        {
+            Console.SetCursorPosition(posX, posY);
+            int lineWidth = (getLargestItem(content) + 4);
 
+            for (int i = 0; i < 5; i++) {
+                if (i == 0 || i == 4) {
+                    Console.WriteLine(borderLine(lineWidth));
+                } else if (i == 1 || i == 3) {
+                    Console.SetCursorPosition(posX, (Console.CursorTop));
+                    Console.WriteLine(paddingLine(lineWidth));
+                }
+                else {
+                    foreach (string temp in content) {
+                        Console.SetCursorPosition(posX, (Console.CursorTop));
+                        if (content[0] == temp)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.WriteLine(conentLine(lineWidth, temp));
+                            Console.ResetColor();
+                            Console.WriteLine(paddingLine(lineWidth));
+                            
+                            
+                        }
+                        else {
+                            Console.WriteLine(conentLine(lineWidth, temp));
+                        }
+                        
+                    }
+                }
+                
+            }
+        }
+
+        private string conentLine(int width, string input) {
+            string result = "";
+            string padding = "";
+            int paddingWidth = ((width - input.Length) - 2)/2;
+
+            for (int i = 0; i < paddingWidth; i++) {
+                padding = padding + " ";
+            }
+
+            result = "*" + padding + input + padding + "*";
+
+            if (result.Length > width) {
+                result = "*" + padding.Substring(0) + input + padding + "*";
+            } else if (result.Length < width) {
+                result = "* " + padding + input + padding + "*";
+            }
+
+            return result;
+        }
+
+        private string paddingLine(int lineWidth) {
+            string result = "";
+
+            for (int i = 0; i < lineWidth; i++) {
+                if (i == 0 || i == (lineWidth - 1))
+                {
+                    result = result + "*";
+                }
+                else {
+                    result = result + " ";
+                }
+            }
+
+            return result;
+        }
+
+        private string borderLine(int width) {
+            string returnString = "";
+
+            for (int i = 0; i < width; i++) {
+                returnString = returnString + "*";
+            }
+
+            return returnString;
+        }
+
+        
+
+        private int getLargestItem(List<string> input) {
+            int returnInt = 0;
+
+            foreach (string item in input) {
+                if (item.Length > returnInt) {
+                    returnInt = item.Length;
+                }
+            }
+
+            return returnInt;
         }
     }
 }
